4a39612a9507f2936bd225160b8bbdcd054348f9
Nick Strayer <nick@Nicks-MacBook-Pro.local>
2017-08-23 16:49:23 -0400

    changed tag pallet and also fixed problem if user entered more unique tags than we have unique colors

--- inst/htmlwidgets/jsDev/src/reducers/tagColorsReducer.js
+++ inst/htmlwidgets/jsDev/src/reducers/tagColorsReducer.js
@@ -1,31 +1,28 @@
-import {uniq, map} from 'rambda';
+import { uniq, map } from 'rambda';
 
 // pallet from colorbrewer.
 const tagColors = [
-  '#a6cee3',
-  '#1f78b4',
-  '#b2df8a',
-  '#33a02c',
-  '#fb9a99',
-  '#e31a1c',
-  '#fdbf6f',
+  '#e41a1c',
+  '#377eb8',
+  '#4daf4a',
+  '#984ea3',
   '#ff7f00',
-  '#cab2d6',
-  '#6a3d9a',
-  '#ffff99',
-  '#b15928',
+  '#ffff33',
+  '#a65628',
+  '#f781bf',
+  '#999999'
 ];
 
 export default (state = {}, action) => {
   switch (action.type) {
     case 'ADD_TAG':
       // get list of unique tags
-      const {tag} = action.tag;
+      const { tag } = action.tag;
       const uniqueTags = Object.keys(state);
       const oldTag = uniqueTags.includes(tag);
       // debugger;
       return !oldTag
-        ? {...state, [tag]: tagColors[uniqueTags.length]}
+        ? { ...state, [tag]: tagColors[uniqueTags.length % tagColors.length] }
         : state;
     default:
       return state;
